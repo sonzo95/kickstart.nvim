4d7a996203fb1015689290ccae2fa1d768a6e7bf
Stefano Sonzogni <stefano.sonzogni@prima.it>
2025-02-13 12:27:13 +0100

    remove swift from formatters and add elixir + fixes

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -1,20 +1,18 @@
 return {
   'stevearc/conform.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
     local conform = require 'conform'
     conform.setup {
       formatters_by_ft = {
-        -- INFO: install via `brew install swiftformat`
-        swift = { 'swiftformat' },
+        elixir = { 'mix' }
       },
+      format_on_save = {
+        enabled = true
+      },
+      formatters = {
+        mix = {},
+      },
+      async = true,
     }
-
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = '*',
-      callback = function(args)
-        require('conform').format { bufnr = args.buf }
-      end,
-    })
   end,
 }
