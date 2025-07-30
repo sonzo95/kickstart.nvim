f47a0c03daa154708568059a3eed93e887ee32d9
Stefano Sonzogni <stefano.sonzogni@prima.it>
2025-07-30 14:51:50 +0200

    update fade

--- lua/custom/plugins/windows.lua
+++ lua/custom/plugins/windows.lua
@@ -2,5 +2,11 @@ return {
   {
     'tadaa/vimade',
     event = 'VeryLazy',
+    fadelevel = 0.8,
+    blocklist = {
+      block_by_filetype = {
+        buf_opts = { bufname = { 'neo-tree', 'trouble' } },
+      },
+    },
   },
 }
