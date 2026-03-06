5f4710882e5be2ef52422194c205da5747118825
Stefano Sonzogni <sonzo995@gmail.com>
2026-03-06 09:41:49 +0100

    add luasnip

---
+++ lua/custom/plugins/lsp/snippets.lua
@@ -0,0 +1,37 @@
+local M = { 'L3MON4D3/LuaSnip' }
+M.dependencies = { 'rafamadriz/friendly-snippets' }
+M.version = 'v2.*'
+M.build = 'make install_jsregexp'
+function M.config()
+  local ls = require 'luasnip'
+  ls.setup { enable_autosnippets = true }
+  require('luasnip.loaders.from_vscode').lazy_load()
+  require('luasnip.loaders.from_lua').load {
+    paths = { './lua/snippets' },
+  }
+
+  require('which-key').add {
+    {
+      mode = 'i',
+      lhs = '<M-n>',
+      rhs = function()
+        if ls.choice_active() then
+          ls.change_choice(1)
+        end
+      end,
+      desc = 'Next choice (luasnip)',
+    },
+    {
+      mode = 'i',
+      lhs = '<M-p>',
+      rhs = function()
+        if ls.choice_active() then
+          ls.change_choice(-1)
+        end
+      end,
+      desc = 'Previous choice (luasnip)',
+    },
+  }
+end
+
+return M
