1a05bca19353452380270097375dfeacd24c5f1b
Stefano Sonzogni <stefano.sonzogni@prima.it>
2026-03-14 12:46:13 +0100

    dialyzer works multiline

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -1,4 +1,41 @@
 local inspect = require 'vim.inspect'
+
+local function commit_item(item, commit_diagnostic, diagnostics, qf_list)
+  -- ONLY attach the diagnostic if the error belongs to the file you are currently looking at
+  if commit_diagnostic then
+    table.insert(diagnostics, {
+      source = 'dialyzer',
+      lnum = item.lnum - 1, -- Neovim's diagnostic API expects 0-indexed line numbers!
+      col = item.col - 1,
+      message = item.message,
+      severity = vim.diagnostic.severity.WARN, -- Dialyzer issues are treated as warnings
+    })
+  end
+
+  table.insert(qf_list, {
+    filename = item.file,
+    lnum = item.lnum,
+    col = item.col,
+    text = item.message,
+    type = 'W',
+  })
+end
+
+local function reset_item(item)
+  item.file = nil
+  item.lnum = nil
+  item.col = 0
+  item.message = nil
+end
+
+local function trim(s)
+  return s:match '^%s*(.-)%s*$'
+end
+
+local function ends_with(str, ending)
+  return ending == '' or str:sub(-#ending) == ending
+end
+
 return {
 
   { -- Linting
@@ -12,7 +49,7 @@ return {
         -- python = { 'flake8' },
         elixir = { 'dialyxir' },
         php = { 'phpstan' },
-        --INFO: Install `brew install shaderc`
+        -- INFO: Install `brew install shaderc`
         glsl = { 'glslc' },
       }
 
@@ -30,7 +67,9 @@ return {
           -- This ensures we can match it against Dialyzer's output.
           local buf_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ':.')
 
-          -- Loop through every line of the stderr output
+          local item = {}
+          reset_item(item)
+
           for _, line in ipairs(vim.split(output, '\n')) do
             -- Matches format: "lib/thresher/my_file.ex:14:1: The call..."
             -- And fallbacks to format: "lib/thresher/my_file.ex:14: The call..."
@@ -40,25 +79,28 @@ return {
               file, lnum, message = string.match(line, '^(.+):(%d+):%s*(.+)$')
             end
 
-            -- ONLY attach the diagnostic if the error belongs to the file you are currently looking at
-            if file and file == buf_name then
-              table.insert(diagnostics, {
-                source = 'dialyzer',
-                lnum = tonumber(lnum) - 1, -- Neovim's diagnostic API expects 0-indexed line numbers!
-                col = tonumber(col or '0'),
-                message = message,
-                severity = vim.diagnostic.severity.WARN, -- Dialyzer issues are treated as warnings
-              })
-            end
+            if file and lnum and message then
+              item.file = file
+              item.lnum = tonumber(lnum)
+              item.message = message
+              if col then
+                item.col = tonumber(col)
+              end
 
-            if file then
-              table.insert(qf_list, {
-                filename = file,
-                lnum = tonumber(lnum),
-                col = tonumber(col or '0'),
-                text = message,
-                type = 'W',
-              })
+              if ends_with(message, '.') then
+                commit_item(item, item.file == buf_name, diagnostics, qf_list)
+                reset_item(item)
+              end
+            else
+              if item.message then
+                local trimmed_line = trim(line)
+                if trimmed_line == '.' then
+                  commit_item(item, item.file == buf_name, diagnostics, qf_list)
+                  reset_item(item)
+                else
+                  item.message = item.message .. '\n' .. line
+                end
+              end
             end
           end
 
