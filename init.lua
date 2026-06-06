58b0ceee9720531705630e453e58fad09787cc2a
Stefano Sonzogni <sonzo995@gmail.com>
2026-06-06 09:28:40 +0200

    fix tree sitter impl for nvim 0.12

--- init.lua
+++ init.lua
@@ -373,46 +373,16 @@ require('lazy').setup({
     lazy = false,
     build = ':TSUpdate',
     branch = 'main',
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      -- Treesitter directory
-      local treesitter_dir = vim.fn.stdpath 'data' .. '/lazy/nvim-treesitter/'
-
-      -- Collect all available parsers
-      local parsers = {}
-      for name, type in vim.fs.dir(treesitter_dir .. 'runtime/queries') do
-        if type == 'directory' then
-          table.insert(parsers, name)
-        end
-      end
-
-      -- Install file type parsers
-      require('nvim-treesitter').install(parsers)
-
-      -- Register known file types
-      dofile(treesitter_dir .. 'plugin/filetypes.lua')
-
-      -- Get file types
-      local file_types = vim
-        .iter(parsers)
-        :map(function(parser)
-          return vim.treesitter.language.get_filetypes(parser)
-        end)
-        :flatten()
-        :totable()
-      -- Auto-run
       vim.api.nvim_create_autocmd('FileType', {
-        pattern = file_types,
         callback = function(args)
-          -- Highlights
-          vim.treesitter.start()
-
-          -- Folds
-          vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          vim.wo[0][0].foldmethod = 'expr'
-
-          -- Indentation
-          vim.bo[args.buf].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+          ---@diagnostic disable-next-line: undefined-field
+          local lang = args.filetype and vim.treesitter.language.get_lang(args.filetype)
+          if lang then
+            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            vim.wo.foldmethod = 'expr'
+            vim.bo[args.buf].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+          end
         end,
       })
     end,
