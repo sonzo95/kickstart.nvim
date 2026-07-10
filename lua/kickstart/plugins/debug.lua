7079a82cbb70ee07a6b615ac375fa3fe270548a7
Stefano Sonzogni <stefano.sonzogni@prima.it>
2026-07-10 17:43:33 +0200

    setup python debugger

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -21,6 +21,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'mfussenegger/nvim-dap-python',
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -78,6 +79,7 @@ return {
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
+    local dap_python = require 'dap-python'
 
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
@@ -93,6 +95,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         -- "delve"
+        'debugpy',
       },
     }
 
@@ -142,5 +145,7 @@ return {
     --     detached = vim.fn.has 'win32' == 0,
     --   },
     -- }
+
+    dap_python.setup(vim.fn.stdpath 'data' .. '/mason/packages/debugpy/venv/bin/python')
   end,
 }
